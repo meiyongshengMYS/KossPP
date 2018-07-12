d7260cbb2e609a2c63196927503a228beb70b4cc
meiysh <meiysh@tingshan.cn>
2018-07-12 14:52:52 +0800

    #1 begin

---
+++ KOSSPP/KOSSPP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KOSSPP
+//
+//  Created by 梅 on 2018/7/12.
+//  Copyright © 2018年 mei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
