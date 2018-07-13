172e7151e5b838599eadfcd1dd0c958760d85f3d
meiysh <meiysh@tingshan.cn>
2018-07-13 09:52:51 +0800

    #4 OK

---
+++ KossPP/KossPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KossPP
+//
+//  Created by 梅 on 2018/7/13.
+//  Copyright © 2018年 mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KossPP/KossPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KossPP
+//
+//  Created by 梅 on 2018/7/13.
+//  Copyright © 2018年 mei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
